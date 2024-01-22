212ada19501af229723a544cc1d3019fbb683b05
hp123789 <45138135+hp123789@users.noreply.github.com>
2024-01-22 11:18:55 -0800

    Update alsa_test.c

--- alsa_test.c
+++ alsa_test.c
@@ -2,12 +2,48 @@
 #include <stdlib.h>
 #include <string.h>
 #include <hiredis/hiredis.h>
+#include <alsa/asoundlib.h>
+#include <unistd.h>
+
+#define FS 30000
+#define CHUNK_SIZE (FS * 0.01)
+
+void setVolume(long volume) {
+    long min, max;
+    snd_mixer_t *handle;
+    snd_mixer_selem_id_t *sid;
+    const char *card = "default";
+    const char *selem_name = "Master";
+
+    snd_mixer_open(&handle, 0);
+    snd_mixer_attach(handle, card);
+    snd_mixer_selem_register(handle, NULL, NULL);
+    snd_mixer_load(handle);
+
+    snd_mixer_selem_id_alloca(&sid);
+    snd_mixer_selem_id_set_index(sid, 0);
+    snd_mixer_selem_id_set_name(sid, selem_name);
+    snd_mixer_elem_t* elem = snd_mixer_find_selem(handle, sid);
+
+    snd_mixer_selem_get_playback_volume_range(elem, &min, &max);
+    snd_mixer_selem_set_playback_volume_all(elem, volume * max / 100);
+
+    snd_mixer_close(handle);
+}
 
 int main (int argc, char **argv) {
     redisReply *reply;
     redisContext *c;
     redisReply *data;
     float *pcmData;
+
+    nice(-20);
+
+    snd_pcm_t *handle;
+    snd_pcm_open(&handle, "default", SND_PCM_STREAM_PLAYBACK, 0);
+    snd_pcm_set_params(handle, SND_PCM_FORMAT_FLOAT_LE, SND_PCM_ACCESS_RW_INTERLEAVED, 1, FS, 0, 50000);
+
+    setVolume(15);
 
     c = redisConnect("192.168.150.2", 6379);
     if (c->err) {
@@ -25,16 +61,17 @@ int main (int argc, char **argv) {
             size_t dataSize = data->len;
             pcmData = (float *)data->str;
             for (int j=0;j<sizeof(pcmData)/sizeof(*pcmData);j++) {
-                 pcmData[j] = (pcmData[j]/(float)50000);
-                 if (pcmData[j] > 1) {
-                     pcmData[j] = (float)1;
-                 }
-                 if (pcmData[j] < -1) {
-                     pcmData[j] = (float)-1;
-                 }
-                printf("%f,", pcmData[j]);
+                pcmData[j] = (pcmData[j]/(float)50000);
+                if (pcmData[j] > 1) {
+                    pcmData[j] = (float)1;
+                }
+                if (pcmData[j] < -1) {
+                    pcmData[j] = (float)-1;
+                }
+                // printf("%f,", pcmData[j]);
             }
-            printf("\n");
+            snd_pcm_writei(handle, pcmData, CHUNK_SIZE);
+            // printf("\n");
             freeReplyObject(reply);
         }
     }
